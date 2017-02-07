e548abe8782da6c053c7e8feee6048487eaab16c
Deep Tailor <deeptailor@gmail.com>
2017-02-07 02:22:45 -0800

    add router and factory

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('flapperNews', ['ui.router']);
+
+
+app.factory('posts', [function(){
+  var o = {
+    posts: []
+  };
+  return o;
+}])
 
 app.controller('MainCtrl' , [
-  '$scope', function($scope){
+  '$scope',
+  'posts',
+  function($scope, posts){
     $scope.test = "Hello World!";
-    $scope.posts = [
-      {title: 'post 1', upvotes: 5},
-      {title: 'post 2', upvotes: 2},
-      {title: 'post 3', upvotes: 15},
-      {title: 'post 4', upvotes: 9},
-      {title: 'post 5', upvotes: 4}
-    ];
+    $scope.posts = posts.posts;
+
     $scope.addPost = function(){
       if(!$scope.title || $scope.title === ''){return;}
       $scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0});
@@ -20,4 +25,19 @@ app.controller('MainCtrl' , [
     $scope.incrementUpvotes = function(post){
       post.upvotes++;
     };
+}]);
+
+app.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: '/home.html',
+        controller: 'MainCtrl'
+      });
+
+    $urlRouterProvider.otherwise('home');
 }]);
