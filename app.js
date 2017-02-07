6f5f166197a7dc13f11c7f6cb0af9c1d747d9c10
Deep Tailor <deeptailor@gmail.com>
2017-02-07 13:09:16 -0800

    init database

--- app.js
+++ app.js
@@ -17,13 +17,40 @@ app.controller('MainCtrl' , [
 
     $scope.addPost = function(){
       if(!$scope.title || $scope.title === ''){return;}
-      $scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0});
+      $scope.posts.push({
+        title: $scope.title,
+        link: $scope.link,
+        upvotes: 0,
+        comments: [
+          {author: 'Joe', body: 'Cool post!', upvotes: 0},
+          {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+        ]
+      });
       $scope.title = '';
       $scope.link = '';
     };
 
     $scope.incrementUpvotes = function(post){
       post.upvotes++;
+    };
+
+}]);
+
+app.controller('PostsCtrl', [
+  '$scope',
+  '$stateParams',
+  'posts',
+  function($scope, $stateParams, posts){
+    $scope.post = posts.posts[$stateParams.id];
+
+    $scope.addComment = function(){
+      if($scope.body === '') { return; }
+      $scope.post.comments.push({
+        body: $scope.body,
+        author: 'user',
+        upvotes: 0
+      });
+      $scope.body = '';
     };
 }]);
 
@@ -38,6 +65,13 @@ app.config([
         templateUrl: '/home.html',
         controller: 'MainCtrl'
       });
+
+      $stateProvider
+        .state('posts', {
+          url: '/posts/{id}',
+          templateUrl: '/posts.html',
+          controller: 'PostsCtrl'
+        });
 
     $urlRouterProvider.otherwise('home');
 }]);
